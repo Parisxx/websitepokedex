1c296b1471293425020b7792660cb4e725473d20
= <505825@vistacollege.nl>
2022-01-17 14:40:39 +0100

    hello

---
+++ add.php
@@ -0,0 +1,44 @@
+<?php
+   session_start();
+   require_once("db_connection.php");
+   $pdo = connect();
+ 
+
+  $stmt = $pdo->query("SELECT * FROM pokemon");
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokedex</title>
+    <link rel="stylesheet" href="styles.css">
+
+    <link rel="icon" href="./fotos/pokeball.png">
+
+</head>
+<body>
+<div class="header">
+   <a href="index.php">
+      <img src="./fotos/pokeball.png"  class="pokeball" alt="pokeball">
+    </a>
+      <h1 id="headertext">Pokedex</h1> 
+     </div>
+
+<div class="pokemon">
+
+
+    
+</div>
+    
+
+<div class="footer">
+    <p> Paris Stassen, Robin Reinders, Vigo Lutke Schipholt
+    <p> @copyright2022
+</div>
+</body>
+</html>
