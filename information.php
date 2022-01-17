bc770ba484da1859ec9b3d1f4e95415a4b497837
= <505825@vistacollege.nl>
2022-01-17 13:34:07 +0100

    dgd

--- information.php
+++ information.php
@@ -26,24 +26,28 @@
     <img src="./fotos/pokeball.png"  class="pokeball" alt="pokeball">
       <h1 id="headertext">Pokedex</h1> 
      </div>
-
-<div class="pokemon">
-
+     <br>
+     <p>Informatie
+     <div class="pokemon">
     <?php
-     while ($data = $stmt->fetch())
+    while ($data = $stmt->fetch())
       {
-
+         
             echo "<h2>". $data['entry'] . "</h2>";
             echo "<h1>" . $data['name'] ."</h4>";
             echo "<h3> " . $data['type'] . "</h3>";
 
             echo "<a href='information.php'>";
             echo "<img src='./fotos/" . $data['photo'] . "' width='300' />";
-            echo "</a>"; 
+            echo "</a>";
+    
+
+          
       }
     ?>
     
-</div>
+    </div>
+    
 
 <div class="footer">
     <p> Paris Stassen, Robin Reinders, Vigo Lutke Schipholt
