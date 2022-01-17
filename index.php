357e82aa0a36ca0ae3b02512a207f26409cc5059
= <505825@vistacollege.nl>
2022-01-17 13:06:35 +0100

    rgdg

--- index.php
+++ index.php
@@ -26,11 +26,27 @@
     <img src="./fotos/pokeball.png"  class="pokeball" alt="pokeball">
       <h1 id="headertext">Pokedex</h1> 
      </div>
-    <p> Ivysaur
+     <br>
+
+    <?php
+    while ($data = $stmt->fetch())
+      {
+          echo "<div class='pokemon'>";
+            echo "<h2>". $data['entry'] . "</h2>";
+            echo "<h1>" . $data['name'] ."</h4>";
+            echo "<h3> " . $data['type'] . "</h3>";
+            echo "<img src='./fotos/" . $data['photo'] . "' width='300' />";
+
+          echo "</div>";
+
+          
+      }
+    ?>
     
 
 <div class="footer">
-  <p> @copyright2022
+    <p> Paris Stassen, Robin Reinders, Vigo Lutke Schipholt
+    <p> @copyright2022
 </div>
 </body>
 </html>
