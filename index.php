3e44f9352b835a40b1ebc4c80002ec76843d0303
= <505825@vistacollege.nl>
2022-01-17 09:35:26 +0100

    bhdth

--- index.php
+++ index.php
@@ -8,4 +8,16 @@
 	header('Location: '.$uri.'/dashboard/');
 	exit;
 ?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokedex</title>
+    
+</head>
+<body>
+    
+</body>
+</html>
