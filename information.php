1700c0917d98876b483be077cf889175f5e56495
= <505825@vistacollege.nl>
2022-01-18 13:47:26 +0100

    hello

--- information.php
+++ information.php
@@ -47,16 +47,34 @@
     <?php
      while ($data = $stmt->fetch())
       {
+        echo "<div class='information1'>";
         echo "<h2>". $data['entry'] . "</h2>";
         echo "<h1>" . $data['name'] ."</h1>";
-        echo "<img src='./fotos/" . $data['photo'] . "' width='300' />";
+        echo "<img src='./fotos/" . $data['photo'] . "' width='400' />";
+        echo "</div>";
+
+        echo "<div class='information2'>";
+        echo "<br>";
+        echo "<br>";
         echo "<p> Type: " . $data['type'] . "</p>";
         echo "<p> Weakness: " . $data['weakness'] . "</p>";
         echo "<p> Category: " . $data['category'] ."</p>";
         echo "<p> Abilities: " . $data['abilities'] . "</p>";
+        echo "<br>";
+        echo "<br>";
         echo "<p> Height: " . $data['height'] ."</p>";
         echo "<p> Weight: " . $data['weight'] ."</p>";
         echo "<p> Gender: " . $data['gender'] . "</p>";
+        echo "</div>";
+        
+        echo "<div class='evolution'>";
+        echo "<h2> Evolutions</h2>";
+        echo "<img src='./fotos/" . $data['evo1'] . "' width='200' />";
+        echo "<img src='./fotos/arrow.png' width='50'  class='arrow' />";
+        echo "<img src='./fotos/" . $data['evo2'] . "' width='200' />";
+        echo "<img src='./fotos/arrow.png' width='50' class='arrow' />";
+        echo "<img src='./fotos/" . $data['evo3'] . "' width='200' />";
+        echo "</div>";
 
       }
     ?>
